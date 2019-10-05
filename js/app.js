8754b45680606ed01010f83a71a7bf44d1fcfbbe
Eliese Gudaitis <eliesefg511@gmail.com>
2019-10-05 10:58:42 -0400

    Editing the jquery
    the jquery is in, not sure why it isn't working

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-$(document).foundation()
+$(document).foundation();
+
+$('nav a').click(function(event){
+  
+  // prevent defalt click behavior
+  // don't jump to content
+  event.preventDefault();
+
+  // remove "selected" class from all links
+  $('nav a').removeClass('selected');
+
+  // add "selected" class to the link that was just selected
+  $(this).addClass('selected');
+    
+  // identify position of target
+  var target = $(this).attr('href');
+  var top = $(target).offset().top;
+
+  // animate scroll to the target
+  $('html,body').animate({
+    scrollTop: top
+    },700);
+
+});
+
+// I tried writing this code for a couple hours by hand
+// it sucked and I couldn't get the hang of it so I just
+// Copy pasted it
